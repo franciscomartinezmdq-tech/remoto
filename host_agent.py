bbb34f25a4ad1fe837e5a094ba93508b0c4999c7
franciscomartinezmdq-tech <franciscomartinezmdq@gmail.com>
2026-03-26 17:36:02 -0300

    Update host_agent.py

--- host_agent.py
+++ host_agent.py
@@ -112,21 +112,9 @@ log.info("--- Todos los imports OK ---")
 RTC_CONFIG = RTCConfiguration(iceServers=[
     RTCIceServer(urls=["stun:stun.l.google.com:19302"]),
     RTCIceServer(urls=["stun:stun1.l.google.com:19302"]),
-    RTCIceServer(
-        urls=["turn:openrelay.metered.ca:80"],
-        username="openrelayproject",
-        credential="openrelayproject",
-    ),
-    RTCIceServer(
-        urls=["turn:openrelay.metered.ca:443"],
-        username="openrelayproject",
-        credential="openrelayproject",
-    ),
-    RTCIceServer(
-        urls=["turn:openrelay.metered.ca:443?transport=tcp"],
-        username="openrelayproject",
-        credential="openrelayproject",
-    ),
+    RTCIceServer(urls=["stun:stun2.l.google.com:19302"]),
+    RTCIceServer(urls=["stun:stun3.l.google.com:19302"]),
+
 ])
 
 
